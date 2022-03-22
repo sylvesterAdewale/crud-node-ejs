15e458664c4221c7933be397113809cff15e42ef
sylvesterAdewale <81014263+sylvesterAdewale@users.noreply.github.com>
2022-03-22 03:57:42 +0100

    Update reportController.js

--- controllers/reportController.js
+++ controllers/reportController.js
@@ -4,7 +4,7 @@ module.exports.newreport_post = async (req, res) => {
     const {tCrime, nOffender, mOffender, lIncident, dDescription} = req.body;
 
     try {
-        const report = await Report.create({tCrime, nOffender, mOffender, lIncident, dDescription});
+        const report = await Report.create({tCrime, nOffender, mOffender, lIncident, dDescription, isResolved});
         res.status(201).json(report)
     } catch (err) {
         console.log(err)
